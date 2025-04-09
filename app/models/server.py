1448663e0156e09c8d09cc50b45ba24e7054991f
Eric Zhang <ericzhang456@disroot.org>
2025-04-09 16:34:37 +0800

    add server db model

---
+++ app/models/server.py
@@ -0,0 +1,11 @@
+from app.extensions import db
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Server(db.Model):
+    server_steam_id: Mapped[int] = mapped_column(primary_key=True)
+    server_name: Mapped[str]
+    server_map: Mapped[str]
+    max_players: Mapped[int]
+    vac: Mapped[bool]
+    sourcetv: Mapped[bool]
