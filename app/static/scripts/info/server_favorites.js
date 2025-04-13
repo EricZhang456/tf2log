a41997610a14e323cd321e410bf5c01dfe17af8f
Eric Zhang <ericzhang456@disroot.org>
2025-04-14 00:36:30 +0800

    fix favroites script in info view

--- app/static/scripts/info/server_favorites.js
+++ app/static/scripts/info/server_favorites.js
@@ -1,6 +1,5 @@
 const favoriteScriptTag = document.currentScript;
 const favoriteButton = document.querySelector(".info_header_favorite");
-const favoritedServersStorage = window.localStorage.getItem("favorited_servers");
 favoriteButton.style.display = "block";
 
 const serverIP = favoriteScriptTag.getAttribute("data-server-ip");
@@ -10,31 +9,42 @@ const serverObj = {server_ip: serverIP, server_port: serverPort};
 let duplicateServer = false;
 let favoritedServers;
 
-if (favoritedServersStorage === null) {
-    favoritedServers = new Array();
-} else {
-    favoritedServers = JSON.parse(favoritedServersStorage);
+const getFavoritedServers = () => {
+    const favoritedServersStorage = window.localStorage.getItem("favorited_servers");
+    if (favoritedServersStorage === null || JSON.parse(favoritedServersStorage).length === 0) {
+        favoritedServers = new Array();
+    } else {
+        favoritedServers = JSON.parse(favoritedServersStorage);
+    }
 }
 
 const checkFavoriteDuplicate = () => {
-    favoritedServers.some((item) => {
-        if (item.server_ip === serverObj.server_ip && item.server_port === serverObj.server_port) {
-            duplicateServer = true;
-            favoriteButton.innerHTML = "Remove from favorites";
-        } else {
-            duplicateServer = false;
-            favoriteButton.innerHTML = "Favorite";
-        }
-    });
+    getFavoritedServers();
+    if (favoritedServers.length) {
+        favoritedServers.some((item) => {
+            if (item.server_ip === serverObj.server_ip && item.server_port === serverObj.server_port) {
+                duplicateServer = true;
+                favoriteButton.innerHTML = "Remove from favorites";
+            } else {
+                duplicateServer = false;
+                favoriteButton.innerHTML = "Favorite";
+            }
+        });
+    } else {
+        duplicateServer = false;
+        favoriteButton.innerHTML = "Favorite";
+    }
 };
 
 const addServerToFavorite = () => {
+    getFavoritedServers();
     if (!duplicateServer) {
         favoritedServers.push(serverObj);
+        window.localStorage.setItem("favorited_servers", JSON.stringify(favoritedServers));
     } else {
-        favoritedServers = favoritedServers.filter(item => item.server_ip !== serverObj.server_ip && item.server_port !== serverObj.server_port);
+        filteredFavorites = favoritedServers.filter(item => item.server_ip !== serverObj.server_ip && item.server_port !== serverObj.server_port);
+        window.localStorage.setItem("favorited_servers", JSON.stringify(filteredFavorites));
     }
-    window.localStorage.setItem("favorited_servers", JSON.stringify(favoritedServers));
     checkFavoriteDuplicate();
 };
 
