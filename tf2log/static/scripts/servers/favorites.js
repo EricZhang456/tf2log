e7154eff28535f2c7b13eac38d58093372c095bc
Eric Zhang <ericzhang456@disroot.org>
2025-05-17 16:52:13 +0800

    semicolon

--- tf2log/static/scripts/servers/favorites.js
+++ tf2log/static/scripts/servers/favorites.js
@@ -63,4 +63,4 @@ function attachFavoriteEventListener() {
 document.addEventListener("DOMContentLoaded", () => attachFavoriteEventListener());
 
 const observer = new MutationObserver(() => attachFavoriteEventListener());
-observer.observe(document.querySelector(".target"), {childList: true})
+observer.observe(document.querySelector(".target"), { childList: true });
