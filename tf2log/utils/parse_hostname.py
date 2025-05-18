499c48638fb717f06544a3594515f79925d2406b
Eric Zhang <ericzhang456@disroot.org>
2025-05-18 22:02:39 +0800

    fix parse_hostname type hinting

--- tf2log/utils/parse_hostname.py
+++ tf2log/utils/parse_hostname.py
@@ -2,13 +2,13 @@
 
 from urllib.parse import urlsplit
 
-def parse_hostname(server_addr: str) -> tuple[str, str]:
+def parse_hostname(server_addr: str) -> tuple[str | None, int | None]:
     """Parse server address and format into a tuple.
     
     :param str server_addr: Server address (in host:port format).
     :return: A tuple of the parsed address, first item being the hostname
                 and second item being the port.
-    :rtype: tuple[str, str]
+    :rtype: tuple[str | None , int | None]
     """
     parsed = urlsplit("//" + server_addr)
     return parsed.hostname, parsed.port
